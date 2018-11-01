e9b0bfdfab96b90b54d6c82fb17f80369cc91c2e
gallopelado <juanftp100@gmail.com>
2018-11-01 08:53:48 -0300

    first commit

---
+++ controlador/PostulacionCandidatoControlador.php
@@ -0,0 +1,29 @@
+<?php
+
+include '../datos/CiudadDao.php';
+
+class CiudadControlador {
+
+  public function obtenerCiudad() {
+    return CiudadDao::getCiudad();
+    CiudadDao::desconectar();
+  }
+
+  public function registrarCiudad($op, $id, $descripcion) {
+    $obj_ciudad = new Ciudad();
+    $obj_ciudad->setOp($op);
+    $obj_ciudad->setId($id);
+    $obj_ciudad->setDescripcion($descripcion);
+
+    return CiudadDao::altaCiudad($obj_ciudad);
+  }
+
+  public function obtenerDescripcion($descripcion) {
+    $obj_ciudad =  new Ciudad();
+    $obj_ciudad->setDescripcion($descripcion);
+
+    return CiudadDao::getDescripcion($obj_ciudad);
+  }
+
+}
+?>
